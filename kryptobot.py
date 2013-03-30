e5ad0fda4f97d0b63588d11928bb18720a12c0df
Freddie Sanchez <freddie.sanchez@gmail.com>
2013-03-29 21:36:51 -0700

    fixed testing support issue

--- kryptobot.py
+++ kryptobot.py
@@ -65,8 +65,8 @@ class KryptoBot(irc.IRCClient):
             return
         user = user.split("!")[0]
         # Otherwise check to see if it is a message directed at me
-        if msg.startswith(self.nickname + ":") or msg.startswith("!"):
-            func,args = self.decipher_cmd(channel,user,msg.split("!")[1].strip()) 
+        if msg.startswith(self.nickname + ":") or msg.startswith(":"):
+            func,args = self.decipher_cmd(channel,user,msg.split(":")[1].strip()) 
             if func != None:
               func(user,channel,args)
             return
